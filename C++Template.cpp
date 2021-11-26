74be2433b9a1bc60dc8717071b7b3b76fb3f89e6
Phạm Huy Cường Thịnh <85171754+teddythinh@users.noreply.github.com>
2021-11-26 10:55:12 +0700

    Update some libraries.

--- C++Template.cpp
+++ C++Template.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <ctime>
+#include <fstream>
+#include <algorithm>
+
 using namespace std;
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
