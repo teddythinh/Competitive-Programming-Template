22a7c8f734ffae5867176a44253d6ce565be82b3
Phạm Huy Cường Thịnh <85171754+teddythinh@users.noreply.github.com>
2022-01-21 22:41:40 +0700

    Update stringstream.cpp

--- stringstream.cpp
+++ stringstream.cpp
@@ -4,19 +4,20 @@
 using namespace std;
 
 int main(){
-    // Define string variables
     string a, temp1, temp2;
-    // Define number variables
-    int ans;
     cin >> a;
+       
     // Create stringstream to split string
     stringstream ss(a);
+   
     // Split string (If string is separate by some characters like "+, - , x, :, |, ?, /" )
     // Initialize 2 variables temp1 and temp2 to store the string you want
     getline(ss, temp1, '+');
     getline(ss, temp2);
+   
     // The example string is 1+1 or 2+2 and the result is 2 or 4
     // Use stoi(int) or stof(float) or stod(double) function to change string to number
+    int ans;
     ans = stoi(temp1) + stoi(temp2);
     cout << ans << endl;
 }
